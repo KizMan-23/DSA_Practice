466ab55175127bd923aa90a971b6536909a5a529
KizMan-23 <kizitougwu777@gmail.com>
2025-04-09 01:47:26 +0100

    refreshing after awhile

--- graph_tree.py
+++ graph_tree.py
@@ -1,3 +1,138 @@
+
+#TREE ALGO..
+class binary_tree():
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.content = None
+        self.right = None
+    
+    def insert(self, value, content = None):
+        if value < self.value: 
+            if self.left is None:
+                self.left = binary_tree(value)
+                self.left.content = content
+            else:
+                self.left.insert(value, content)
+        else:
+            if self.right is None:
+                self.right = binary_tree(value)
+                self.right.content = content
+            else:
+                self.right.insert(value, content)
+        
+    def inorder_traverse(self):
+        # to go as far left as available at every right
+        #returns in increasing order
+        if self.left:
+            self.left.inorder_traverse()
+        print(self.value)
+        if self.right:
+            self.right.inorder_traverse()
+
+    def preorder_traverse(self):
+        print(self.value)
+        if self.left:
+            self.left.preorder_traverse()
+        
+        if self.right:
+            self.right.preorder_traverse()
+
+
+    def postorder_traverse(self):
+        if self.left:
+            self.left.postorder_traverse()
+        
+        if self.right:
+            self.right.postorder_traverse()
+        print(self.value)
+
+    def find(self, value):
+        if value < self.value:
+            if self.left is None:
+                return None
+            else:
+                return self.left.find(value)
+        elif value > self.value:
+            if self.right is None:
+                return None
+            else:
+                return self.right.find(value)
+        else:
+            return self
+
+bts = binary_tree(10)
+
+# bts.insert(10)
+bts.insert(5, content= {"data": "Hello World"})  #To store and append data in tree node storage
+bts.insert(4)
+bts.insert(2)
+bts.insert(1)
+bts.insert(3)
+bts.insert(22)
+bts.insert(11)
+bts.insert(12)
+
+
+print(bts.left.left.left.right.value)
+
+print(bts.find(5).content['data'])
+
+
+
+class Tree_Node():
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class binary_search_tree():
+    def __init__(self):
+        self.root = None
+        # self.data = None
+    
+    def insert(self, data):
+        tr_node = Tree_Node(data)
+        if not self.root:
+            self.root = tr_node
+        
+        if data < tr_node.data:                    # '<' not supported between instances of 'Tree_Node' and 'int'
+            if tr_node.left is None:
+                tr_node.left = Tree_Node(data)
+            else:
+                tr_node.left.insert(data)
+        else:
+            if tr_node.right is None:
+                tr_node.right = Tree_Node(data)
+            else:
+                tr_node.right.insert(data)
+
+    def inorder_traversal(self):
+        tr_node = Tree_Node(self)
+        if tr_node.left:
+            tr_node.left.inorder_traversal ()
+        print(tr_node.left.data)
+        if tr_node.right:
+            tr_node.right.inorder_traversal() 
+     
+    def __str__(self):
+        pass
+
+
+bin = binary_search_tree()
+bin.insert(5)
+bin.insert(10)
+bin.insert(4)
+bin.insert(3)
+bin.insert(11)
+bin.insert(22)
+bin.insert(15)
+bin.insert(2)
+bin.insert(3)
+bin.insert(1)
+
+print(bin.inorder_traversal())
+
 #Q1: check if the path exists between src & dst (directed acyclic graph. i.e--one path). return true or false
 #Sample graph
 #queue Algorithm = Breath-First-Search(BFS)
@@ -78,83 +213,3 @@ def unique_province(edges):
         return
 
     return count #count should be 4
-
-
-#TREE ALGO..
-class binary_tree():
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.content = None
-        self.right = None
-    
-    def insert(self, value, content = None):
-        if value < self.value: 
-            if self.left is None:
-                self.left = binary_tree(value)
-                self.left.content = content
-            else:
-                self.left.insert(value, content)
-        else:
-            if self.right is None:
-                self.right = binary_tree(value)
-                self.right.content = content
-            else:
-                self.right.insert(value, content)
-        
-    def inorder_traverse(self):
-        # to go as far left as available at every right
-        #returns in increasing order
-        if self.left:
-            self.left.inorder_traverse()
-        print(self.value)
-        if self.right:
-            self.right.inorder_traverse()
-
-    def preorder_traverse(self):
-        print(self.value)
-        if self.left:
-            self.left.preorder_traverse()
-        
-        if self.right:
-            self.right.preorder_traverse()
-
-
-    def postorder_traverse(self):
-        if self.left:
-            self.left.postorder_traverse()
-        
-        if self.right:
-            self.right.postorder_traverse()
-        print(self.value)
-
-    def find(self, value):
-        if value < self.value:
-            if self.left is None:
-                return None
-            else:
-                return self.left.find(value)
-        elif value > self.value:
-            if self.right is None:
-                return None
-            else:
-                return self.right.find(value)
-        else:
-            return self
-
-bts = binary_tree(10)
-
-# bts.insert(10)
-bts.insert(5, content= {"data": "Hello World"})  #To store and append data in tree node storage
-bts.insert(4)
-bts.insert(2, {"data": "Hello World"})
-bts.insert(1)
-bts.insert(3, {"data": "Hello World"})
-bts.insert(22)
-bts.insert(11, {"data": "Hello World"})
-bts.insert(12)
-
-
-print(bts.left.left.left.right.value)
-
-print(bts.find(5).content['data'])
--- sorting_algos.py
+++ sorting_algos.py
@@ -5,11 +5,30 @@
 #merge sort
 #shell sort
 
-lpt = -1
-size = 8
-for i in range(1):
-    print(f"{i}", end=" - > ")
-print("END")
+dic = {"data": "first line",
+       "comp_1": "another instance",
+       "comp_2": "building a list"}
 
-mod = 8 % 9
-print(mod)
+
+# for idx, element in enumerate(dic):
+#     print(f"the index is of {idx} while the key is {element}")
+#     print(f"'{dic[element]}', is the content of this index")
+
+
+dic_3 = {
+    "A": {0: None},
+    "B": {2: "A"},
+    "C": {5: "A"},
+    "D": {7: "B"},
+    "E": {8: "C"}
+}
+
+for idx, element in enumerate(dic_3):
+    # print(f"'{dic_3[element]}', is the content of this index")
+
+    for a, b in enumerate(dic_3[element]):
+        print(f"{b} is the distance from {element} to {dic_3[element][b]}")
+
+
+for ky, val in dic_3.items():
+    print(f"using items function to print key of {ky} with values {val}")
