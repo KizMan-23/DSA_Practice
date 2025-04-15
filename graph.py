4744f04fe274e8a958d88b09a37ae720563225b2
KizMan-23 <kizitougwu777@gmail.com>
2025-04-15 02:53:33 +0100

    working on graph structure

--- graph.py
+++ graph.py
@@ -17,18 +17,21 @@ class ad_Graph():
         return self.matrix[src][dst] == 1 
 
     def __str__(self):
+        result  = ""
         for d in self.nodes:
-            print(f"  {d}", end="")
-        print("")
+            result += f"  {d}"
+        result += "\n"
 
         for a in range(self.size):
-            print(f"{self.nodes[a]}", end="")
+            result += f"{self.nodes[a]}"
         
             for b in self.matrix[a]:
-                print(f" {b}", end=" ")   
+                result += f" {b} "   
 
-            print("")
+            result += "\n"
         #__str__ must have a return statement
+        return result
+    
 
 new_graph = ad_Graph(5)
 
@@ -46,9 +49,9 @@ new_graph.addEdge(2, 4)
 new_graph.addEdge(4, 2)
 new_graph.addEdge(4, 0)
 
-# print(new_graph.checkEdge(0,1))
-# print(new_graph.checkEdge(3,4))
-# print(new_graph)
+print(new_graph.checkEdge(0,1))
+print(new_graph.checkEdge(3,4))
+print(new_graph)
 
 class Node:
     def __init__(self, data):
@@ -91,6 +94,7 @@ class Adj_Graph:
             result.append(f"Vertex {i}: {' -> '.join(adj_str)}")
         return '\n'.join(result)
 
+
 new_agraph = Adj_Graph(5)
 
 new_agraph.addNode("A")
@@ -107,6 +111,94 @@ new_agraph.addEdge(2, 4)
 new_agraph.addEdge(4, 2)
 new_agraph.addEdge(4, 0)
 
-# print(new_agraph.checkEdge(0,1))
-# print(new_agraph.checkEdge(3,4))
+print(new_agraph.checkEdge(0,1))
+print(new_agraph.checkEdge(3,4))
 print(new_agraph)
+
+
+class Graph():
+    def __init__(self, directed = False):
+        self.directed = directed
+        self.adj_list = dict()
+
+    def __repr__(self):
+        graph_str = ""
+
+        for node, neighbors in self.adj_list:
+            graph_str += f"{node} -> {neighbors}\n"
+        return graph_str 
+
+    def add_node(self, node):
+        if node not in self.adj_list:
+            self.adj_list[node] = set()
+        else:
+            raise ValueError("Node Already Exists")
+
+    def remove_node(self, node):
+        if node not in self.adj_list:
+            raise ValueError("Node does Not Exist")
+        else:
+            for neighbor in self.adj_list.values():
+                neighbor = neighbor.discard(node)
+            
+            del self.adj_list[node]
+            # self.adj_list[node] = None
+            # del node
+
+    def add_edge(self, src_node, dst_node, weighted = None):
+        if src_node not in self.adj_list:
+            self.adj_list.add_node(src_node)
+        
+        if dst_node not in self.adj_list:
+            self.adj_list.add_node(dst_node)
+
+        if weighted is None:
+            self.adj_list[src_node].add(dst_node) #maybe use append??
+            if not self.directed:
+                self.adj_list[dst_node].add(src_node)
+        else:
+            self.adj_list[src_node].add((dst_node, weighted))
+            if not self.directed:
+                self.adj_list[dst_node].add((src_node, weighted))
+
+
+    def remove_edge(self, src_node, dst_node):
+        if src_node in self.adj_list:
+            if dst_node in self.adj_list[src_node]:
+                self.adj_list[src_node].remove(dst_node)  #remove may not be a list function
+            else:
+                raise ValueError(f"Node {dst_node} does not exist in Source Node")
+            
+            if not self.directed:
+                if src_node in self.adj_list[dst_node]:
+                    self.adj_list[dst_node].remove(src_node)
+                else:
+                    raise ValueError(f"Source Node may not exist in Node {dst_node}")
+        else:
+            raise ValueError(f"Node {src_node} does not exist in the Adjacency List")
+
+    def get_neighbors(self, node):
+        result = set()
+        for neigbors in self.adj_list[node]:
+            result.add(neigbors)
+        return result
+
+        # return self.adj_list.get(node,set())
+
+    def has_node(self, node):
+        return node in self.adj_list
+
+    def has_edge(self, src_node, dst_node):
+        pass
+
+    def get_nodes(self):
+        pass
+
+    def get_edges(self):
+        pass
+
+    def bfs(self, start):
+        pass
+
+    def dfs(self, start):
+        pass
